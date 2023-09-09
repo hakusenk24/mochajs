1c395f47300d16e75a5cdcf0e67b7e8b671b06a0
hakusenk24 <hakusen.k24@gmail.com>
2023-09-09 11:18:39 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Mochajs
+
+### Run unit tests
+
+```
+./node_modules/mocha/bin/mocha.js test/
+```
+Or
+
+```
+npm run test
+```
---
+++ test/test2.js
@@ -0,0 +1,12 @@
+var assert = require('assert');
+
+describe('Test2.Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+    it('should return the index when the value is present', function () {
+      assert.equal([1, 2, 3].indexOf(3), 2);
+    });
+  });
+});
